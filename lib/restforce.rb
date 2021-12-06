9af32f9b4704a73bed7cad4c5f8213c6c453feb8
mhusnain <waqas@datalatics.com>
2021-12-06 13:41:15 +0500

    resource not found error fix by moving class to uper level

---
+++ lib/restforce.rb
@@ -0,0 +1,104 @@
+# frozen_string_literal: true
+
+require 'faraday'
+require 'faraday_middleware'
+require 'json'
+
+require 'restforce/version'
+require 'restforce/config'
+
+module Restforce
+  autoload :AbstractClient, 'restforce/abstract_client'
+  autoload :SignedRequest,  'restforce/signed_request'
+  autoload :Collection,     'restforce/collection'
+  autoload :Middleware,     'restforce/middleware'
+  autoload :Attachment,     'restforce/attachment'
+  autoload :Document,       'restforce/document'
+  autoload :UploadIO,       'restforce/upload_io'
+  autoload :SObject,        'restforce/sobject'
+  autoload :Client,         'restforce/client'
+  autoload :Mash,           'restforce/mash'
+
+  module Concerns
+    autoload :Authentication, 'restforce/concerns/authentication'
+    autoload :Connection,     'restforce/concerns/connection'
+    autoload :Picklists,      'restforce/concerns/picklists'
+    autoload :Streaming,      'restforce/concerns/streaming'
+    autoload :Caching,        'restforce/concerns/caching'
+    autoload :Canvas,         'restforce/concerns/canvas'
+    autoload :Verbs,          'restforce/concerns/verbs'
+    autoload :Base,           'restforce/concerns/base'
+    autoload :API,            'restforce/concerns/api'
+    autoload :BatchAPI,       'restforce/concerns/batch_api'
+  end
+
+  module Data
+    autoload :Client, 'restforce/data/client'
+  end
+
+  module Tooling
+    autoload :Client, 'restforce/tooling/client'
+  end
+
+  Error               = Class.new(StandardError)
+  ServerError         = Class.new(Error)
+  AuthenticationError = Class.new(Error)
+  UnauthorizedError   = Class.new(Error)
+  APIVersionError     = Class.new(Error)
+  BatchAPIError       = Class.new(Error)
+
+  # Inherit from Faraday::Error::ResourceNotFound for backwards-compatibility
+  # Consumers of this library that rescue and handle Faraday::Error::ResourceNotFound
+  # can continue to do so.
+  NotFoundError       = Class.new(Faraday::ResourceNotFound)
+
+  # Inherit from Faraday::Error::ClientError for backwards-compatibility
+  # Consumers of this library that rescue and handle Faraday::Error::ClientError
+  # can continue to do so.
+  ResponseError       = Class.new(Faraday::Error::ClientError)
+  MatchesMultipleError= Class.new(ResponseError)
+  EntityTooLargeError = Class.new(ResponseError)
+
+  module ErrorCode
+    def self.const_missing(constant_name)
+      const_set constant_name, Class.new(ResponseError)
+    end
+  end
+
+  class << self
+    # Alias for Restforce::Data::Client.new
+    #
+    # Shamelessly pulled from https://github.com/pengwynn/octokit/blob/master/lib/octokit.rb
+    def new(*args, &block)
+      data(*args, &block)
+    end
+
+    def data(*args, &block)
+      Restforce::Data::Client.new(*args, &block)
+    end
+
+    def tooling(*args, &block)
+      Restforce::Tooling::Client.new(*args, &block)
+    end
+
+    # Helper for decoding signed requests.
+    def decode_signed_request(*args)
+      SignedRequest.decode(*args)
+    end
+  end
+
+  # Add .tap method in Ruby 1.8
+  module CoreExtensions
+    def tap
+      yield self
+      self
+    end
+  end
+  Object.include Restforce::CoreExtensions unless Object.respond_to? :tap
+end
+
+if ENV['PROXY_URI']
+  warn "[restforce] You must now use the SALESFORCE_PROXY_URI environment variable (as " \
+       "opposed to PROXY_URI) to set a proxy server for Restforce. Please update your " \
+       "environment's configuration."
+end
---
+++ lib/restforce/middleware/raise_error.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+module Restforce
+  class Middleware::RaiseError < Faraday::Response::Middleware
+    def on_complete(env)
+      @env = env
+      case env[:status]
+      when 300
+        raise Restforce::MatchesMultipleError.new(
+          "300: The external ID provided matches more than one record",
+          response_values
+        )
+      when 401
+        raise Restforce::UnauthorizedError, message
+      when 404
+        raise Restforce::NotFoundError, message
+      when 413
+        raise Restforce::EntityTooLargeError.new(
+          "413: Request Entity Too Large",
+          response_values
+        )
+      when 400...600
+        klass = exception_class_for_error_code(body['errorCode'])
+        raise klass.new(message, response_values)
+      end
+    end
+
+    def message
+      message = "#{body['errorCode']}: #{body['message']}"
+      message << "\nRESPONSE: #{JSON.dump(@env[:body])}"
+    rescue StandardError
+      message # if JSON.dump fails, return message without extra detail
+    end
+
+    def body
+      @body = (@env[:body].is_a?(Array) ? @env[:body].first : @env[:body])
+
+      case @body
+      when Hash
+        @body
+      else
+        { 'errorCode' => '(error code missing)', 'message' => @body }
+      end
+    end
+
+    def response_values
+      {
+        status: @env[:status],
+        headers: @env[:response_headers],
+        body: @env[:body]
+      }
+    end
+
+    ERROR_CODE_MATCHER = /\A[A-Z_]+\z/.freeze
+
+    def exception_class_for_error_code(error_code)
+      return Restforce::ResponseError unless ERROR_CODE_MATCHER.match?(error_code)
+
+      constant_name = error_code.split('_').map(&:capitalize).join.to_sym
+      Restforce::ErrorCode.const_get(constant_name)
+    end
+  end
+end
